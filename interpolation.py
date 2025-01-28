9bb016da0f3da28a2df1a59f7ffb987a4592ec04
melvin-pointdexter <sagigl@ac.sce.ac.il>
2025-01-28 20:45:57 +0200

    feat(interpolation.py): function stubs

---
+++ interpolation.py
@@ -0,0 +1,22 @@
+
+def linear():
+    pass
+
+def polynomial():
+    pass
+
+def cubic_spline():
+    pass
+
+def lagrange():
+    pass
+
+def neville():
+    pass
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
